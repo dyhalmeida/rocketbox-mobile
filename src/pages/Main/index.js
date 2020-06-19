516ffe00c859731322fe2eb23635c9609dd43699
Diego Almeida <dyhalmeida@gmail.com>
2020-06-18 21:44:30 -0300

    added main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import logo from '../../assets/logo.png';
+
+import api from '../../services/api';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class Main extends Component {
+
+    state = {
+        boxName: ''
+    }
+
+    async componentDidMount() {
+        const box = await AsyncStorage.getItem("@rocketbox:box");
+        if (box) {
+            this.props.navigation.navigate("Box");
+        }
+    }
+
+    handleSignIn = async () => {
+        const { data } = await api.post('boxes', { title: this.state.boxName });
+        await AsyncStorage.setItem("@rocketbox:box", data._id);
+        this.props.navigation.navigate("Box");
+    }
+
     render() {
         return (
-            <View>
-                <Text>Olá Diego Almeida</Text>
+            <View style={styles.container}>
+                <Image style={styles.logo} source={logo} />
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Crie um box"
+                    placeholderTextColor="#999"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    underlineColorAndroid="transparent"
+                    value={this.state.boxName}
+                    onChangeText={text => this.setState({ boxName: text })}
+                />
+                <TouchableOpacity
+                    onPress={this.handleSignIn}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText} >Criar</Text>
+                </TouchableOpacity>
             </View>
         )
     }
