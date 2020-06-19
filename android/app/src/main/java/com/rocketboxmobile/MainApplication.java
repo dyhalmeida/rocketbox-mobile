a3e878750351e41fa277c8e478b64c162a6a45a6
Diego Almeida <dyhalmeida@gmail.com>
2020-06-19 15:09:32 -0300

    deleted debug folder android

--- android/app/src/main/java/com/rocketboxmobile/MainApplication.java
+++ android/app/src/main/java/com/rocketboxmobile/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.vinzscam.reactnativefileviewer.RNFileViewerPackage;
 import com.rnfs.RNFSPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
